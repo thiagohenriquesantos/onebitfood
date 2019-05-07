763ff61af0f0d985650d3d079a453c22d13d264f
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-05-07 13:50:20 -0300

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./App.scss";
 import { Container, Section } from 'rbx';
+import Header from "./components/header";
 
 const App = () => (
   <div>
+    <Header />
     <Section>
       <Container>
         One Bit Food
